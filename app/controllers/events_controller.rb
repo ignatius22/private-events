598165949ddb44bb122cedcc8ece032130249984
Ignatius22 <igee54077@gmail.com>
2020-08-10 15:08:51 +0100

    add create actionusing the build method of the association to create the new event with user'sID prepopulated

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,31 @@
 class EventsController < ApplicationController
+
 	def index
 		@events = Event.all
 	end
+
+	def show
+    @event = User.find(params[:id])
+  end
+
+	def new
+		@event = Event.new
+	end
+
+
+  def create
+  	event = Event.new
+    @event = event.build_creator(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:creator)
+  end
 end
