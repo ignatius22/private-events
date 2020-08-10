30a31d25f830748cb7fa951779814d0c88217f0f
Ignatius22 <igee54077@gmail.com>
2020-08-10 05:41:45 +0100

    Create an EventsController and corresponding routes to allow an event to be created

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :new, :show]
+  resources :events, only: [:create, :new, :show, :index]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
